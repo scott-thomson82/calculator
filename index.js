4bba7a493a7d5731da586923c39dd4bd2a0e541e
Scott Thomson <scottthomson_57@hotmail.com>
2019-12-16 15:57:25 +1300

    dessign and JS pseudocode

---
+++ index.js
@@ -0,0 +1,43 @@
+// Create variables that will store DOM elements
+
+
+
+// Create variables that will store input
+
+
+
+// Add event listeners to all buttons
+
+
+
+
+
+//  Create a function that will store the contents of a clicked button and display it.
+
+    
+
+// Append the content of the button to displayVal variable and display it
+
+
+
+// Create a function that will perform the mathematical operation 
+
+
+
+// Create a function that will clear everything
+
+
+
+
+// Create a function that will insert decimal points
+
+
+
+
+
+
+
+
+
+
+
